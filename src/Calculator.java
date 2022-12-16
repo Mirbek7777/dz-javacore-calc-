70356c6bdcf8fe04cbc8ed456b9bc8130a5702aa
Mirbek <mirbek_karimberdiev@mail.ru>
2022-12-16 14:05:33 +0300

    калькулятор комит2

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,12 +1,16 @@
 import java.util.function.*;
 
+import java.util.function.*;
+
 public class Calculator {
-    public static Supplier<Calculator> instance = Calculator::new;
+
+    static Supplier<Calculator> instance = Calculator::new;
 
     BinaryOperator<Integer> plus = (x, y) -> x + y;
     BinaryOperator<Integer> minus = (x, y) -> x - y;
     BinaryOperator<Integer> multiply = (x, y) -> x * y;
-    BinaryOperator<Integer> devide = (x, y) -> x / y;
+    BinaryOperator<Integer> devide = (x, y) -> y == 0 ? 0 : x / y;
+
 
     UnaryOperator<Integer> pow = x -> x * x;
     UnaryOperator<Integer> abs = x -> x > 0 ? x : x * -1;
